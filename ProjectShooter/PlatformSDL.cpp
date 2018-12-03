7ef545fc1a63acf1b0336de8d2c2a0c1c0efd562
OscarScorp <scorpion.co@gmail.com>
2018-12-03 09:02:15 -0600

    Render, glew and glm
    Include Render folder, glew and glm files.

--- ProjectShooter/PlatformSDL.cpp
+++ ProjectShooter/PlatformSDL.cpp
@@ -1,8 +1,15 @@
 #include "pch.h"
 #include "PlatformSDL.h"
 #include "SDL.h"
+#include "GL/glew.h"
 #include <iostream>
 #include <string>
+
+void PlatformSDL::draw()
+{
+	SDL_GL_SwapWindow(window);
+	SDL_Delay(1);
+}
 
 void PlatformSDL::init()
 {
@@ -11,7 +18,8 @@ void PlatformSDL::init()
 	setWidth(getWidth());
 	setHeight(getHeight());
 
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
 		std::cout << "SDL_Init";
 		return;
 	}
@@ -27,20 +35,41 @@ void PlatformSDL::init()
 			break;
 	}
 
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
 	window = SDL_CreateWindow(getName().c_str(), SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED, getWidth(), getHeight(), windowMode);
+		SDL_WINDOWPOS_CENTERED, getWidth(), getHeight(), windowMode | SDL_WINDOW_OPENGL);
+
 	if (window == nullptr) {
 		Log::Write("CreateWindow");
 		throw std::runtime_error("error");
 		return;
 	}
 
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	renderer = SDL_GL_CreateContext(window);
+
 	if (renderer == nullptr) {
 		Log::Write("CreateRenderer");
 		throw std::runtime_error("error");
 		return;
 	}
+
+	GLenum status = glewInit();
+	if (status != GLEW_OK)
+	{
+		std::cout << "error init opengl";
+	}
+
+	glEnable(GL_DEPTH_TEST);
+
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
 }
 
 int PlatformSDL::inputOne()
@@ -52,6 +81,13 @@ int PlatformSDL::inputOne()
 	if (currentKeyStates[SDL_SCANCODE_ESCAPE])
 		return 0;*/
 
+	SDL_Event e;
+	while (SDL_PollEvent(&e))
+	{
+
+	}
+	glClearColor(1, 0, 0, 1);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	return 0;
 }
 
--- ProjectShooter/PlatformSDL.h
+++ ProjectShooter/PlatformSDL.h
@@ -1,15 +1,17 @@
 #pragma once
 #include "Platform.h"
+#include "GL/glew.h"
 #include "SDL.h"
 #include "Log.h"
 class PlatformSDL : public Platform
 {
 private:
 	SDL_Window * window;
-	SDL_Renderer * renderer;
+	SDL_GLContext renderer;
 	int windowMode{ 0 };
 public:
 	void init() override; //+1 punto cora :'v
+	void draw();
 	int inputOne() override;
 	PlatformSDL();
 	~PlatformSDL();
